61698d7f0e95a2104b1da9785960daa4ac424a05
GregRomero <agregromero007@yahoo.com>
2021-04-13 20:34:38 -0700

    fix timer

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 const timeRightDisplay = document.querySelector("#time-right")
 const startBtn = document.querySelector("#start-button")
-let timeRight = 7
+let timeRight = 7;
 
-function countDown(){
-    setInterval (function(){
-        if(timeRight<= 0 ) {
-            clearInterval(timeRight = 0)
+let timer;
+
+function countDown() {
+    timer = setInterval(function () {
+        timeRight--;
+        timeRightDisplay.textContent = timeRight
+        if (timeRight <= 0) {
+            clearInterval(timer)
         }
-    timeRightDisplay.innerHTML = timeRight
-    timeRight -=1
-}, 1000)
+    }, 1000)
 }
 
 let shuffledQuestions, currentQuestionIndex
@@ -25,20 +27,21 @@ nextButton.addEventListener("click", () => {
     setNextQuestion()
 })
 
-function startGame(){
+function startGame() {
+    countDown();
     startButton.classList.add("hide")
-    shuffledQuestions = questions.sort(() => Math.random() -.5)
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove("hide")
     setNextQuestion()
 }
 
-function setNextQuestion(){
+function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question){
+function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
         const button = document.createElement("button")
@@ -67,18 +70,18 @@ function selectAnswer(e) {
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    if (shuffledQuestions.length > currentQuestionIndex + 1){
-    nextButton.classList.remove("hide")
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
     } else {
         startButton.innerText = "Restart"
         startButton.classList.remove("hide")
     }
 }
 
-function setStatusClass(element, correct){
+function setStatusClass(element, correct) {
     clearStatusClass(element)
-    if(correct){
-        element.classList.add("correct")    
+    if (correct) {
+        element.classList.add("correct")
     } else {
         element.classList.add("wrong")
     }
@@ -93,29 +96,29 @@ const questions = [
     {
         question: "What's the biggest animal in the world?",
         answers: [
-            {text:"The blue whale", correct: true},
-            {text: "Elephant", correct: false}
+            { text: "The blue whale", correct: true },
+            { text: "Elephant", correct: false }
         ]
     },
     {
         question: "What does IPA stand for?",
         answers: [
-            {text:"Indian Pale Ale", correct: true},
-            {text: "Indica Pals Alien", correct: false}
+            { text: "Indian Pale Ale", correct: true },
+            { text: "Indica Pals Alien", correct: false }
         ]
     },
     {
         question: "What is the largest country in the world?",
         answers: [
-          { text: "Canada", correct: false },
-          { text: "Russia", correct: true }
+            { text: "Canada", correct: false },
+            { text: "Russia", correct: true }
         ]
-      },
-      {
+    },
+    {
         question: "What is the longest river in the world?",
         answers: [
-          { text: "Mississippi River", correct: false },
-          { text: "River Nile", correct: true }
+            { text: "Mississippi River", correct: false },
+            { text: "River Nile", correct: true }
         ]
-      }
-    ]
+    }
+]
